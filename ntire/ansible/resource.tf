9be490cddd3f79891a65527828adf532fd574419
rupa <rupavadranam0609@gmail.com>
2023-04-06 14:19:56 +0530

    2 nd changes in ansible in terraformtemplet

--- ntire/ansible/resource.tf
+++ ntire/ansible/resource.tf
@@ -77,15 +77,12 @@ resource "azurerm_linux_virtual_machine" "terraformansible" {
   resource_group_name = azurerm_resource_group.resource_group.name
   size                = "Standard_B1s"
   admin_username      = "RUPA_ANAND"
+  admin_password = "rupa@1234567"
   custom_data         = filebase64("ansible.sh")
   network_interface_ids = [
     azurerm_network_interface.web_nic.id,
   ]
 
-  admin_ssh_key {
-    username   = "RUPA_ANAND"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
